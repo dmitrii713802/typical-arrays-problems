dd693a84acad8bd6d2629a082b95cc1729e1f845
dmitrii713802 <71220286+dmitrii713802@users.noreply.github.com>
2020-10-03 23:57:56 +0300

    init: start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        return Math.min(...array);
+    }
+    else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        return Math.max(...array);
+    }
+    else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+        if (array && array.length !== 0)   {
+            let sum = array.reduce((previous, current) => current += previous);
+            let avg = sum / array.length;
+            return avg;
+        }
+        return 0;
 }
+
